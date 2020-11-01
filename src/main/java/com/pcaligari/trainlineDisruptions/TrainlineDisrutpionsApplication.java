ba1f1c13be65a773b6dd1f0ad44980aa74c18935
Paul Caligari <pcaligari68@gmail.com>
2020-11-01 22:28:14 +0000

    Initial Commit - after intellij creates everything

---
+++ src/main/java/com/pcaligari/trainlineDisruptions/TrainlineDisrutpionsApplication.java
@@ -0,0 +1,13 @@
+package com.pcaligari.trainlineDisruptions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainlineDisrutpionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainlineDisrutpionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pcaligari/trainlineDisruptions/TrainlineDisrutpionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pcaligari.trainlineDisruptions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainlineDisrutpionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
