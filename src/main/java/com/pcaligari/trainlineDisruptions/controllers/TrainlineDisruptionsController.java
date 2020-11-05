466cf8b2b08df65fb0df852fb6a162b3827781b6
Paul Caligari <pcaligari68@gmail.com>
2020-11-05 08:03:52 +0000

    refactor for some structure structure - test still passes, yay

--- src/main/java/com/pcaligari/trainlineDisruptions/controllers/TrainlineDisruptionsController.java
+++ src/main/java/com/pcaligari/trainlineDisruptions/controllers/TrainlineDisruptionsController.java
@@ -1,4 +1,4 @@
-package com.pcaligari.trainlineDisruptions;
+package com.pcaligari.trainlineDisruptions.controllers;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
